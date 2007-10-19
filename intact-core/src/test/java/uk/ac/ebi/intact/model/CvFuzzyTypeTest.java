cd94ee1fd1c7859f9c7984480f16bcf673ecc5fe
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-19 10:40:14 +0000

    [IAC-129] Add methods in CvFuzzyType to check if object is less-than, greater-than, range, certain

--- intact-core/src/test/java/uk/ac/ebi/intact/model/CvFuzzyTypeTest.java
+++ intact-core/src/test/java/uk/ac/ebi/intact/model/CvFuzzyTypeTest.java
@@ -72,4 +72,84 @@ public class CvFuzzyTypeTest extends IntactBasicTestCase {
         fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", "foobar" );
         Assert.assertFalse( fuzzyType.isUndetermined() );
     }
+
+    @Test
+    public void isLessThan() throws Exception {
+        CvFuzzyType fuzzyType;
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.LESS_THAN_MI_REF, CvFuzzyType.LESS_THAN );
+        Assert.assertTrue( fuzzyType.isLessThan() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.LESS_THAN_MI_REF, "foobar" );
+        Assert.assertTrue( fuzzyType.isLessThan() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", CvFuzzyType.LESS_THAN );
+        Assert.assertTrue( fuzzyType.isLessThan() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", "foobar" );
+        Assert.assertFalse( fuzzyType.isLessThan() );
+    }
+
+    @Test
+    public void isGreaterThan() throws Exception {
+        CvFuzzyType fuzzyType;
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.GREATER_THAN_MI_REF, CvFuzzyType.GREATER_THAN );
+        Assert.assertTrue( fuzzyType.isGreaterThan() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.GREATER_THAN_MI_REF, "foobar" );
+        Assert.assertTrue( fuzzyType.isGreaterThan() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", CvFuzzyType.GREATER_THAN );
+        Assert.assertTrue( fuzzyType.isGreaterThan() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", "foobar" );
+        Assert.assertFalse( fuzzyType.isGreaterThan() );
+    }
+
+    @Test
+    public void isRange() throws Exception {
+        CvFuzzyType fuzzyType;
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.RANGE_MI_REF, CvFuzzyType.RANGE );
+        Assert.assertTrue( fuzzyType.isRange() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.RANGE_MI_REF, "foobar" );
+        Assert.assertTrue( fuzzyType.isRange() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", CvFuzzyType.RANGE );
+        Assert.assertTrue( fuzzyType.isRange() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", "foobar" );
+        Assert.assertFalse( fuzzyType.isRange() );
+    }
+
+    @Test
+    public void isCertain() throws Exception {
+        CvFuzzyType fuzzyType;
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.CERTAIN_MI_REF, CvFuzzyType.CERTAIN );
+        Assert.assertTrue( fuzzyType.isCertain() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, CvFuzzyType.CERTAIN_MI_REF, "foobar" );
+        Assert.assertTrue( fuzzyType.isCertain() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", CvFuzzyType.CERTAIN );
+        Assert.assertTrue( fuzzyType.isCertain() );
+        Assert.assertFalse( fuzzyType.isNTerminal() );
+
+        fuzzyType = getMockBuilder().createCvObject( CvFuzzyType.class, "MI:xxxx", "foobar" );
+        Assert.assertFalse( fuzzyType.isCertain() );
+    }
 }
