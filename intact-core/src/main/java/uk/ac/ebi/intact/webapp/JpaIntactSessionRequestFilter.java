a246d2a67806460f870b7582ea43269e2bcce972
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-14 19:07:09 +0000

    JpaIntactSessionRequestFilter must allow to exclude extensions

--- intact-core/src/main/java/uk/ac/ebi/intact/webapp/JpaIntactSessionRequestFilter.java
+++ intact-core/src/main/java/uk/ac/ebi/intact/webapp/JpaIntactSessionRequestFilter.java
@@ -32,8 +32,42 @@ public class JpaIntactSessionRequestFilter implements Filter {
 
     private static final Log log = LogFactory.getLog( IntactSessionRequestFilter.class );
 
-    public void init(FilterConfig filterConfig) throws ServletException {
+    private FilterConfig myFilterConfig;
 
+    private static final String FILTERED_PARAM_NAME = "uk.ac.ebi.intact.filter.EXCLUDED_EXTENSIONS";
+
+    private static final String[] DEFAULT_EXCLUDED_EXTENSIONS = new String[]{".js", "logout"};
+
+    private List<String> excludedExtensions;
+
+    public void init( FilterConfig filterConfig ) throws ServletException {
+        log.debug( "Initializing filter..." );
+
+        myFilterConfig = filterConfig;
+
+        excludedExtensions = new ArrayList<String>();
+
+        for ( String defaultNotFilterExt : DEFAULT_EXCLUDED_EXTENSIONS ) {
+            excludedExtensions.add( defaultNotFilterExt );
+        }
+
+        String paramValue = filterConfig.getInitParameter( FILTERED_PARAM_NAME );
+
+        if ( paramValue != null ) {
+            String[] fexts = paramValue.split( "," );
+
+            for ( String fext : fexts ) {
+                fext = fext.trim();
+
+                if ( fext.startsWith( "*" ) ) {
+                    fext = fext.substring( 1, fext.length() );
+                }
+
+                excludedExtensions.add( fext );
+            }
+        }
+
+        log.debug( "Will not create IntactContexts for requests URL ending with: " + excludedExtensions );
     }
 
     public void doFilter( ServletRequest request,
@@ -45,12 +79,29 @@ public class JpaIntactSessionRequestFilter implements Filter {
         HttpServletRequest req = ( HttpServletRequest ) request;
         HttpSession session = req.getSession();
 
+        // This is to prevent the IntactSessionRequestFilter from filtering all urls as for exemple it's not worth
+        // in our case to open and close an hibernate session if the url end in logout, as it means that the user
+        // is just asking to logout the application.
+        String requestUrl = req.getRequestURL().toString();
+        log.debug( "Request send is : " + requestUrl );
+        // if the the url end matches with a filtered extensions do not start IntactContext
+        for ( String ext : excludedExtensions ) {
+            if ( requestUrl.toLowerCase().endsWith( ext.toLowerCase() ) ) {
+                log.debug( "Context not created for (excluded): " + requestUrl );
+                chain.doFilter( request, response );
+                return;
+            }
+        }
+
+
         if (log.isDebugEnabled())
         {
             log.debug( "Creating JPA IntactContext, for request url: " + req.getRequestURL() + " ; session id: "+session.getId() +" ; thread: "+Thread.currentThread().getName());
         }
         IntactSession intactSession = new WebappSession( session.getServletContext(), session, req );
         IntactContext context = IntactConfigurator.createIntactContext( intactSession );
+
+        assert IntactContext.currentInstanceExists();
 
         chain.doFilter( request, response );
     }
