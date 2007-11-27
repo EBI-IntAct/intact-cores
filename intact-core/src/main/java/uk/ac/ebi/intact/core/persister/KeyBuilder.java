5b8db86a524758892b35354f64ee0661302046c5
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-27 11:17:30 +0000

    IAC-192 (Create KeyBuilder class, to create non-AC based keys for the entities)

--- intact-core/src/main/java/uk/ac/ebi/intact/core/persister/KeyBuilder.java
+++ intact-core/src/main/java/uk/ac/ebi/intact/core/persister/KeyBuilder.java
@@ -17,18 +17,36 @@ package uk.ac.ebi.intact.core.persister;
 
 import uk.ac.ebi.intact.model.*;
 import uk.ac.ebi.intact.model.util.CrcCalculator;
+import uk.ac.ebi.intact.model.util.XrefUtils;
+import uk.ac.ebi.intact.persistence.util.CgLibUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Builds string that allow to identify an intact object.
  *
  * @author Samuel Kerrien
+ * @author Bruno Aranda
  * @version $Id$
  * @since 1.8.0
  */
 class KeyBuilder {
 
     public Key keyFor( Institution institution ) {
-        throw new UnsupportedOperationException( );
+        final Collection<InstitutionXref> institutionXrefs = XrefUtils.getIdentityXrefs(institution);
+
+        Key key;
+
+        if (institutionXrefs.isEmpty()) {
+            key = keyForAnnotatedObject(institution);
+        } else {
+            key = new Key("Institution:"+concatPrimaryIds(institutionXrefs));
+        }
+
+        return key;
     }
 
     public Key keyFor( Publication publication ) {
@@ -36,7 +54,7 @@ class KeyBuilder {
     }
 
     public Key keyFor( Experiment experiment ) {
-        throw new UnsupportedOperationException( );
+        return keyForAnnotatedObject(experiment);
     }
 
     public Key keyFor( Interaction interaction ) {
@@ -44,26 +62,80 @@ class KeyBuilder {
     }
 
     public Key keyFor( Interactor interactor ) {
-        throw new UnsupportedOperationException( );
+        final Collection<InteractorXref> interactorXrefs = XrefUtils.getIdentityXrefs(interactor);
+
+        Key key;
+
+        if (interactorXrefs.isEmpty()) {
+            key = keyForAnnotatedObject(interactor);
+        } else {
+            Class normalizedClass = CgLibUtil.removeCglibEnhanced(interactor.getClass());
+            key = new Key(normalizedClass.getSimpleName()+":"+concatPrimaryIds(interactorXrefs));
+        }
+
+        return key;
     }
 
     public Key keyFor( Component component ) {
-        throw new UnsupportedOperationException( );
+        if (component.getInteraction() == null) {
+            throw new IllegalArgumentException("Cannot create a component key for component without interaction: "+component);
+        }
+        if (component.getInteractor() == null) {
+            throw new IllegalArgumentException("Cannot create a component key for component without interactor: "+component);
+        }
+
+        String label = "Component:"+component.getInteraction().getShortLabel()+"_"+component.getInteractor().getShortLabel();
+        return new Key(label);
     }
 
     public Key keyFor( Feature feature ) {
-        throw new UnsupportedOperationException( );
+        if (feature.getComponent() == null) {
+            throw new IllegalArgumentException("Cannot create a feature key for feature without component: "+feature);
+        }
+
+        Key componentKey = keyFor(feature.getComponent());
+
+        return new Key(keyForAnnotatedObject(feature).getUniqueString()+"___"+componentKey.getUniqueString());
     }
 
     public Key keyFor( Range range ) {
-        throw new UnsupportedOperationException( );
+        Key featureKey = keyFor(range.getFeature());
+
+        Key fromFuzzyKey = keyFor(range.getFromCvFuzzyType());
+        Key toFuzzyKey = keyFor(range.getToCvFuzzyType());
+
+        String uniqueString = "Range:"+range.getFromIntervalStart()+"_"+range.getFromIntervalEnd()+"-"+
+                              range.getToIntervalStart()+"_"+range.getToIntervalEnd()+"___"+
+                              fromFuzzyKey.getUniqueString()+"___"+toFuzzyKey.getUniqueString()+"___"+
+                              featureKey.getUniqueString();
+        
+        return new Key(uniqueString);
     }
 
     public Key keyFor( CvObject cvObject ) {
-        throw new UnsupportedOperationException( );
+        return new Key(cvObject.getMiIdentifier());
     }
 
     protected Key keyForAnnotatedObject( AnnotatedObject annotatedObject ) {
-        return new Key(annotatedObject.getShortLabel());
+        Class normalizedClass = CgLibUtil.removeCglibEnhanced(annotatedObject.getClass());
+        return new Key(normalizedClass.getSimpleName()+":"+annotatedObject.getShortLabel());
+    }
+
+    protected String concatPrimaryIds(Collection<? extends Xref> xrefs) {
+         List<String> primaryIds = new ArrayList<String>(xrefs.size());
+
+        for (Xref xref : xrefs) {
+            primaryIds.add(xref.getPrimaryId());
+        }
+
+        Collections.sort(primaryIds);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String primaryId : primaryIds) {
+            sb.append(primaryId).append("___");
+        }
+
+        return sb.toString();
     }
 }
