4904024e4b40b979d7cd15b6f2698d4215b41bb0
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-05-06 06:49:28 +0000

    IAC-276 (IAC-276 (IntactTransaction.wasRolledBack provoking recursive invocation))

--- intact-core/src/main/java/uk/ac/ebi/intact/persistence/dao/JpaIntactTransaction.java
+++ intact-core/src/main/java/uk/ac/ebi/intact/persistence/dao/JpaIntactTransaction.java
@@ -46,6 +46,6 @@ public class JpaIntactTransaction extends IntactTransaction {
     }
 
     public boolean wasRolledBack() {
-        return wasRolledBack();
+        return wasRolledBack;
     }
 }
